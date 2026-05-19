6e307b6075a08a11d44dbdbc6b177ae6c79cc0d0
Vinn278 <orvinkristantyo@gmail.com>
2026-05-19 09:52:11 +0700

    Update welcome.js

--- login/js/welcome.js
+++ login/js/welcome.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     function goLogin() {
-        window.location.href = "login.html";
+        window.location.href = "login/login.html";
     }
 
     function logout() {
