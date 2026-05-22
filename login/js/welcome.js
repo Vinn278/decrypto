42cbd8825cb0f277ade0ca123d53d14893950da9
Vinn278 <orvinkristantyo@gmail.com>
2026-05-22 21:22:00 +0700

    Update welcome.js

--- login/js/welcome.js
+++ login/js/welcome.js
@@ -15,7 +15,14 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("userInfo").innerText = "Halo, " + user;
 
         document.getElementById("authArea").innerHTML = `
-            <button onclick="logout()" class="logout-btn">Logout</button>
+            <button onclick="logout()" class="logout-btn">
+                   <svg viewBox="0 0 24 24" fill="transparent" stroke="currentColor" stroke-width="2">
+                        <path d="M9 21H5a2 2 0 01-2-2V5a2 2 0 012-2h4"/>
+                        <polyline points="16 17 21 12 16 7"/>
+                        <line x1="21" y1="12" x2="9" y2="12"/>
+                    </svg>
+                Logout
+            </button>
         `;
     }
 
