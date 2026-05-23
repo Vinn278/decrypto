7e4ecd22c5516fc69a1b82b2f0350692fd930398
Vinn278 <orvinkristantyo@gmail.com>
2026-05-23 16:38:38 +0700

    Update welcome.js

--- login/js/welcome.js
+++ login/js/welcome.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const user = localStorage.getItem("username");
     if (user) {
         document.getElementById("userInfo").innerText = "Halo, " + user;
-        document.getElementById("userInfoo").innerText = "Selamat datang, " +  user ", ini adalah gambaran umum portofoliomu";
+        document.getElementById("userInfoo").innerText = "Selamat datang, " +  user + ", ini adalah gambaran umum portofoliomu";
 
         document.getElementById("authArea").innerHTML = `
             <button onclick="logout()" class="logout-btn">
